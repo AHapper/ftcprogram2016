35adc0d6e19ac3d139db27698d5bf33f1563fdd0
jameshapper <happer@hotmail.com>
2016-12-07 21:16:52 -0500

    checking git and github with "test change" and now "again"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopTank_Iterative.java
@@ -53,7 +53,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-//test change
+//test change again
 
 @TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
 @Disabled
